39140d6ddc012279820c1bd1036e367d73be4d63
Corentin Mors <corentin@dashlane.com>
2023-08-30 15:54:08 +0200

    Update dashlane-cli to v1.14.0

--- Formula/dashlane-cli.rb
+++ Formula/dashlane-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class DashlaneCli < Formula
   desc "Command-line interface for Dashlane"
   homepage "https://dashlane.com"
-  url "https://github.com/Dashlane/dashlane-cli/archive/refs/tags/v1.13.0.tar.gz"
-  sha256 "f4176d7ff6d57e5b1c6729c5c8234f43525d0efd3bf6371b89cf59a274bebb67"
+  url "https://github.com/Dashlane/dashlane-cli/archive/refs/tags/v1.14.0.tar.gz"
+  sha256 "9aef5c7c92eb8b18314d67aa36768340f502b77456e3e06fb2e19d7d912cce3b"
   license "Apache-2.0"
 
   livecheck do
