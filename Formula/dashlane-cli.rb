71e6b66703e63839c5008b267ae38b32a435204f
Corentin Mors <corentin@dashlane.com>
2024-05-20 12:45:06 +0200

    Update dashlane-cli to v6.2421.0
    Update yarn and pkg.
    Fixes https://github.com/Dashlane/dashlane-cli/issues/215

--- Formula/dashlane-cli.rb
+++ Formula/dashlane-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class DashlaneCli < Formula
   desc "Command-line interface for Dashlane"
   homepage "https://dashlane.com"
-  url "https://github.com/Dashlane/dashlane-cli/archive/refs/tags/v6.2416.0.tar.gz"
-  sha256 "595026a52f8c8021078d0ab166fb50c920ef5fbe35cd539844c387621fe7c91c"
+  url "https://github.com/Dashlane/dashlane-cli/archive/refs/tags/v6.2421.0.tar.gz"
+  sha256 "e196f6bcad521bf92f64cdf5975d62aa9d9b8c2e20d1f1d4531ea1e7c6e7a531"
   license "Apache-2.0"
 
   livecheck do
@@ -12,8 +12,12 @@ class DashlaneCli < Formula
     strategy :github_latest
   end
 
+  option 'with-corepack', 'Use yarn from corepack instead of installing it'
+
   depends_on "node@18" => :build
-  depends_on "yarn" => :build
+  
+  depends_on 'yarn' if !build.with?('corepack')
+  depends_on 'corepack' if  build.with?('corepack')
 
   on_macos do
     # macos 12+ only
@@ -23,13 +27,13 @@ class DashlaneCli < Formula
   def install
     Language::Node.setup_npm_environment
     platform = OS.linux? ? "linux" : "macos"
-    system "yarn", "set", "version", "4.0.2"
+    system "yarn", "set", "version", "4.2.2"
     system "yarn", "install", "--frozen-lockfile"
     system "yarn", "run", "build"
     system "yarn", "workspaces", "focus", "--production"
-    system "yarn", "dlx", "@yao-pkg/pkg@5.11.1", ".",
+    system "yarn", "dlx", "@yao-pkg/pkg@5.11.5", ".",
       "-t", "node18-#{platform}-#{Hardware::CPU.arch}", "-o", "bin/dcli",
-      "--no-bytecode", "--public", "--public-packages", "tslib,thirty-two"
+      "--no-bytecode", "--public", "--public-packages", "tslib,thirty-two,node-hkdf-sync,vows"
     bin.install "bin/dcli"
   end
 
