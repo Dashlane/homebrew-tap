539756967d98905b658e742b60eb0e752d744be4
Corentin Mors <corentin@dashlane.com>
2024-06-10 15:23:59 +0200

    Revert dashlane-cli.rb
    Signed-off-by: Corentin Mors <corentin@dashlane.com>

--- Formula/dashlane-cli.rb
+++ Formula/dashlane-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class DashlaneCli < Formula
   desc "Command-line interface for Dashlane"
   homepage "https://dashlane.com"
-  url "https://github.com/Dashlane/dashlane-cli/archive/refs/tags/v6.2424.0.tar.gz"
-  sha256 "914bf949416521a3823a70847731d16edcc2b37fecf654dc073c0164d5267ba5"
+  url "https://github.com/Dashlane/dashlane-cli/archive/refs/tags/v6.2421.0.tar.gz"
+  sha256 "e196f6bcad521bf92f64cdf5975d62aa9d9b8c2e20d1f1d4531ea1e7c6e7a531"
   license "Apache-2.0"
 
   livecheck do
@@ -36,7 +36,7 @@ class DashlaneCli < Formula
     system "yarn", "install", "--frozen-lockfile"
     system "yarn", "run", "build"
     system "yarn", "workspaces", "focus", "--production"
-    system "yarn", "dlx", "@yao-pkg/pkg@5.12.0", ".",
+    system "yarn", "dlx", "@yao-pkg/pkg@5.11.5", ".",
       "-t", "node18-#{platform}-#{Hardware::CPU.arch}", "-o", "bin/dcli",
       "--no-bytecode", "--public", "--public-packages", "tslib,thirty-two,node-hkdf-sync,vows"
     bin.install "bin/dcli"
